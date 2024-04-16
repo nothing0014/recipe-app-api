25a0317572d73104de014e64eeb906bfdcef38fa
Allen <nothing86617@gmail.com>
2024-04-16 20:49:40 +0800

    Check

--- app/core/admin.py
+++ app/core/admin.py
@@ -9,23 +9,38 @@ from core import models
 
 
 class UserAdmin(BaseUserAdmin):
-    """Define the admin pasges for users."""
+    """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email','name']
-    filedsets = (
+    fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
-                'fields':(
+                'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
                 )
             }
         ),
-        (_('Important dates'),{'fields': ('last_login',)},)
+        (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets= (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 admin.site.register(models.User, UserAdmin)
