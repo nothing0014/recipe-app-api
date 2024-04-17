ca5ca5544d64c5c08300a97a991753b8685eb363
Allen <nothing86617@gmail.com>
2024-04-17 20:55:47 +0800

    C

--- app/user/views.py
+++ app/user/views.py
@@ -22,11 +22,11 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManagerUserView(generics.RetrieveUpdateAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
-    perrmision_classes = [permissions.IsAuthenticated]
+    permision_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         """Retrieve and return the authenticated user."""
