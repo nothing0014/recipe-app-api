d9aba43d5e3898bdccb2d86bab60a29658dead96
Allen <nothing86617@gmail.com>
2024-04-17 20:51:22 +0800

    C

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,11 +22,22 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
     email = serializers.EmailField()
-    password = serializers.EmailField(
+    password = serializers.CharField(
         style={'input_type':'password'},
         trim_whitespace=False,
     )
@@ -40,7 +51,7 @@ class AuthTokenSerializer(serializers.Serializer):
             username=email,
             password=password,
         )
-        if not iser:
+        if not user:
             msg=_('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
 
