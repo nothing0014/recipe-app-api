820b7c95d7dceb7e7820c517503d9ed7e58863bb
Allen <nothing86617@gmail.com>
2024-04-17 20:08:20 +0800

    C

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,12 +1,16 @@
 """
 Serializers for the user API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 
-from rest_framework import Serializers
+from rest_framework import serializers
 
 
-class UserSerializer(Serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
     class Meta:
@@ -17,3 +21,28 @@ class UserSerializer(Serializers.ModelSerializer):
     def create(self, validated_data):
         """Create and return a user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+    email = serializers.EmailField()
+    password = serializers.EmailField(
+        style={'input_type':'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not iser:
+            msg=_('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user']=user
+        return attrs
